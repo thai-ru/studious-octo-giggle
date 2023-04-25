ec8c97f4ee2e891d15e5577996231d00f91866e2
thai-ru <robinthairu34@gmail.com>
2023-04-25 07:14:00 -0400

    Connected fake API to generate user list

--- pages/ninja/index.tsx
+++ pages/ninja/index.tsx
@@ -1,10 +1,33 @@
-const Ninjas = () => {
+export const getStaticProps = async() => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+
+    return {
+
+        props:{ ninjas:data }
+
+    }
+
+}
+
+const Ninjas = ( {ninjas} ) => {
     return ( 
         <>
-        <div>
+        <div className="p-4">
             <p className="text-3xl font-bold" >
                 All Ninjas
             </p>
+            <div>
+                {ninjas.map( ninja =>(
+                    <div key={ninja.id} >
+                        <a>
+                            <h3 className="my-4 hover:border-l-4 border-rose-900 bg-white p-4 text-lg " >{ ninja.name }</h3>
+                        </a>
+
+                    </div>
+                ) )}
+            </div>
         </div>
         </>
      );
